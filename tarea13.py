f2990af7979dc2d9db4d7ec0d588f8de26711a04
jefferson silva <85767459+jeffersonsilva876@users.noreply.github.com>
2021-06-27 10:53:19 -0500

    Add files via upload

---
+++ tarea13.py
@@ -0,0 +1,18 @@
+#Elabore pseudocódigo para el caso en que se desean 
+#escribir los números del 1 al 100."""
+
+class Tarea13:
+    def __init__ (self):
+        pass
+    def Variables(self):
+        
+        print("numeros hasta el 100 con While")
+        i=1
+        while i <=100:
+            print(i)
+            i=i+1
+                       
+        input("fin") 
+        
+resultado = Tarea13()
+resultado.Variables()

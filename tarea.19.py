79273a9564faf24ada116f3ee44aa617186d8f60
jefferson silva <85767459+jeffersonsilva876@users.noreply.github.com>
2021-06-27 18:29:53 -0500

    Add files via upload

---
+++ tarea.19.py
@@ -0,0 +1,35 @@
+
+#Se tiene información sobre las calificaciones de 6 exámenes de un grupo de 30 alumnos. Los datos sobre estos exámenes se proporcionan de la siguiente 
+#manera
+
+class Tarea19:
+    def __init__(self):
+        pass
+    def vector(self):
+        for i=1 in 30 :
+            for j==1 in 6:
+            print ("Escriba la calificación del alumno",i, "en el examen",j)
+            i,j=input ("ingrese las calificaciones de estudiantes")  
+        for j==1 in 6 :
+            sum = 0
+            for  i==1 in 30 :
+                sum = sum + i,j
+            prom = sum/30
+            print("promedio examen",j , prom)
+        for  i==1 in 30 :
+            sum = 0
+            for j==1 in 6 :
+                sum = sum + i,j
+            print("promedio del alumno",i,sum/6)
+        Examen = 1
+        promayor = prom[1]
+        for  j==2  in 6:
+            if promayor < prom :
+                promayor = prom
+                Examen = j
+        print ("el examen", Examen, "obtuvo el mayor promedio= ", promayor)
+                                   
+        input("fin")
+
+tarea=Tarea19()
+tarea.vector()
